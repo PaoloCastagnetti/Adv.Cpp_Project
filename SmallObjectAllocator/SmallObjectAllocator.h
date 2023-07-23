2f8b737b692bfc47aecd8db19ba6cbee5200a836
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-07-23 19:49:49 +0200

    SmallObjectAllocator definition

--- SmallObjectAllocator/SmallObjectAllocator.h
+++ SmallObjectAllocator/SmallObjectAllocator.h
@@ -1,10 +1,18 @@
 #pragma once
 
 #include "framework.h"
+#include "FixedAllocator.h"
 #include <string>
 #include <vector>
 
 class SMALLOBJECTALLOCATOR_API SmallObjectAllocator {
-public:
-	SmallObjectAllocator();
+	public:
+		SmallObjectAllocator(std::size_t chunkSize, std::size_t maxObjectSize);
+		void* Allocate(std::size_t numBytes);
+		void Deallocate(void* p, std::size_t size);
+
+	private:
+		std::vector<FixedAllocator> pool_;
+		FixedAllocator* pLastAlloc_;
+		FixedAllocator* pLastDealloc_;
 };
