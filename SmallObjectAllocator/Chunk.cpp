d55ffa686d87eedca62141dc1eef7fcbb1504544
Paolo Castagnetti <castagnettipaolo01@gmail.com>
2023-09-13 11:57:31 +0200

    Chunk commented for documentation

--- SmallObjectAllocator/Chunk.cpp
+++ SmallObjectAllocator/Chunk.cpp
@@ -1,16 +1,22 @@
-/**
- * @file Chunk.cpp
- * @brief Implementation file for the Chunk class.
- */
-
 #include "Chunk.h"
 
- /**
-  * @brief Initializes the Chunk with the specified block size and number of blocks.
-  * Allocates memory for the blocks and sets up the block allocation list.
-  * @param blockSize The size of each block in bytes.
-  * @param blocks The number of blocks in the Chunk.
-  */
+#pragma region Inline functions
+
+inline bool Chunk::HasBlock(void* p, ::std::size_t chunkLength) const {
+    unsigned char* pc = static_cast<unsigned char*>(p);
+    return (pData_ <= pc) && (pc < pData_ + chunkLength);
+}
+
+inline bool Chunk::HasAvailable(unsigned char numBlocks) const {
+    return (blocksAvailable_ == numBlocks);
+}
+
+inline bool Chunk::IsFilled(void) const {
+    return (0 == blocksAvailable_);
+}
+
+#pragma endregion
+
 bool Chunk::Init(std::size_t blockSize, unsigned char blocks) {
 	assert(blockSize > 0);
 	assert(blocks > 0);
@@ -26,12 +32,6 @@ bool Chunk::Init(std::size_t blockSize, unsigned char blocks) {
 	return true;
 }
 
-/**
- * @brief Allocates a block of memory from the Chunk.
- * If there are no available blocks, returns nullptr.
- * @param blockSize The size of the block to allocate in bytes.
- * @return A pointer to the allocated block, or nullptr if no blocks are available.
- */
 void* Chunk::Allocate(std::size_t blockSize) {
 	if (IsFilled())
 		return nullptr;
@@ -43,11 +43,6 @@ void* Chunk::Allocate(std::size_t blockSize) {
 	return pResult;
 }
 
-/**
- * @brief Deallocates a block of memory in the Chunk and makes it available for reuse.
- * @param p A pointer to the block to deallocate.
- * @param blockSize The size of the block in bytes.
- */
 void Chunk::Deallocate(void* p, std::size_t blockSize) {
 	assert(p >= pData_);
 	unsigned char* toRelease = static_cast<unsigned char*>(p);
@@ -67,7 +62,7 @@ void Chunk::Deallocate(void* p, std::size_t blockSize) {
 	++blocksAvailable_;
 }
 
-void Chunk::Reset(::std::size_t blockSize, unsigned char blocks) {
+void Chunk::Reset(std::size_t blockSize, unsigned char blocks) {
 	assert(blockSize > 0);
 	assert(blocks > 0);
 	// Overflow check
@@ -81,109 +76,48 @@ void Chunk::Reset(::std::size_t blockSize, unsigned char blocks) {
 		*p = ++i;
 	}
 }
+
 void Chunk::Release() {
 	assert(nullptr != pData_);
 	std::free(static_cast<void*>(pData_));
 }
 
-inline bool Chunk::HasBlock(void* p, ::std::size_t chunkLength) const {
-	unsigned char* pc = static_cast<unsigned char*>(p);
-	return (pData_ <= pc) && (pc < pData_ + chunkLength);
-}
-
-inline bool Chunk::HasAvailable(unsigned char numBlocks) const {
-	return (blocksAvailable_ == numBlocks);
-}
-
-inline bool Chunk::IsFilled(void) const {
-	return (0 == blocksAvailable_);
-}
-
-bool Chunk::IsCorrupt(unsigned char numBlocks, ::std::size_t blockSize,
-    bool checkIndexes) const {
+bool Chunk::IsCorrupt(unsigned char numBlocks, ::std::size_t blockSize, bool checkIndexes) const {
 
     if (numBlocks < blocksAvailable_) {
-        // Contents at this Chunk corrupted.  This might mean something has
-        // overwritten memory owned by the Chunks container.
         assert(false);
         return true;
     }
     if (IsFilled())
-        // Useless to do further corruption checks if all blocks allocated.
         return false;
     unsigned char index = firstAvailableBlock_;
     if (numBlocks <= index) {
-        // Contents at this Chunk corrupted.  This might mean something has
-        // overwritten memory owned by the Chunks container.
         assert(false);
         return true;
     }
     if (!checkIndexes)
-        // Caller chose to skip more complex corruption tests.
         return false;
 
-    /* If the bit at index was set in foundBlocks, then the stealth index was
-     found on the linked-list.
-     */
     std::bitset< UCHAR_MAX > foundBlocks;
     unsigned char* nextBlock = nullptr;
 
-    /* The loop goes along singly linked-list of stealth indexes and makes sure
-     that each index is within bounds (0 <= index < numBlocks) and that the
-     index was not already found while traversing the linked-list.  The linked-
-     list should have exactly blocksAvailable_ nodes, so the for loop will not
-     check more than blocksAvailable_.  This loop can't check inside allocated
-     blocks for corruption since such blocks are not within the linked-list.
-     Contents of allocated blocks are not changed by Chunk.
-
-     Here are the types of corrupted link-lists which can be verified.  The
-     corrupt index is shown with asterisks in each example.
-
-     Type 1: Index is too big.
-      numBlocks == 64
-      blocksAvailable_ == 7
-      firstAvailableBlock_ -> 17 -> 29 -> *101*
-      There should be no indexes which are equal to or larger than the total
-      number of blocks.  Such an index would refer to a block beyond the
-      Chunk's allocated domain.
-
-     Type 2: Index is repeated.
-      numBlocks == 64
-      blocksAvailable_ == 5
-      firstAvailableBlock_ -> 17 -> 29 -> 53 -> *17* -> 29 -> 53 ...
-      No index should be repeated within the linked-list since that would
-      indicate the presence of a loop in the linked-list.
-     */
     for (unsigned char cc = 0; ; ) {
         nextBlock = pData_ + (index * blockSize);
         foundBlocks.set(index, true);
         ++cc;
         if (cc >= blocksAvailable_)
-            // Successfully counted off number of nodes in linked-list.
             break;
         index = *nextBlock;
         if (numBlocks <= index) {
-            /* This catches Type 1 corruptions as shown in above comments.
-             This implies that a block was corrupted due to a stray pointer
-             or an operation on a nearby block overran the size of the block.
-             */
             assert(false);
             return true;
         }
         if (foundBlocks.test(index)) {
-            /* This catches Type 2 corruptions as shown in above comments.
-             This implies that a block was corrupted due to a stray pointer
-             or an operation on a nearby block overran the size of the block.
-             Or perhaps the program tried to delete a block more than once.
-             */
             assert(false);
             return true;
         }
     }
     if (foundBlocks.count() != blocksAvailable_) {
-        /* This implies that the singly-linked-list of stealth indexes was
-         corrupted.  Ideally, this should have been detected within the loop.
-         */
         assert(false);
         return true;
     }
@@ -191,15 +125,13 @@ bool Chunk::IsCorrupt(unsigned char numBlocks, ::std::size_t blockSize,
     return false;
 }
 
-bool Chunk::IsBlockAvailable(void* p, unsigned char numBlocks,
-    ::std::size_t blockSize) const {
+bool Chunk::IsBlockAvailable(void* p, unsigned char numBlocks, std::size_t blockSize) const {
     (void)numBlocks;
 
     if (IsFilled())
         return false;
 
     unsigned char* place = static_cast<unsigned char*>(p);
-    // Alignment check
     assert((place - pData_) % blockSize == 0);
     unsigned char blockIndex = static_cast<unsigned char>(
         (place - pData_) / blockSize);
@@ -209,9 +141,6 @@ bool Chunk::IsBlockAvailable(void* p, unsigned char numBlocks,
     if (index == blockIndex)
         return true;
 
-    /* If the bit at index was set in foundBlocks, then the stealth index was
-     found on the linked-list.
-     */
     ::std::bitset< UCHAR_MAX > foundBlocks;
     unsigned char* nextBlock = nullptr;
     for (unsigned char cc = 0; ; ) {
@@ -219,7 +148,6 @@ bool Chunk::IsBlockAvailable(void* p, unsigned char numBlocks,
         foundBlocks.set(index, true);
         ++cc;
         if (cc >= blocksAvailable_)
-            // Successfully counted off number of nodes in linked-list.
             break;
         index = *nextBlock;
         if (index == blockIndex)
