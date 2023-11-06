821b9af61689169359705225c35d0e9f5bd2ea97
Marco Pastro <marco.pastrelloo@gmail.com>
2023-11-06 14:05:25 +0100

    BigObjectAllocator and changes to the RedBlackTree

---
+++ GeneralAllocator/BigObjectAllocator.h
@@ -0,0 +1,92 @@
+#pragma once
+#include <iostream>
+#include "RedBlackTree.hpp"
+#include <cstddef>
+#include <vector>
+
+class FreeBlock {
+public:
+    char* address;
+    size_t size;
+    FreeBlock* pre;
+    FreeBlock* next;
+    FreeBlock() : pre(nullptr), next(nullptr) {}
+    ~FreeBlock() {
+        if (next)
+            delete next;
+    }
+};
+class BigObjectAllocator {
+public:
+    /*
+        @brief
+        create the block of memory, the RedBlackTree and the list
+        @param totalSize the total size of the memory block
+    */
+    BigObjectAllocator(size_t totalSize);
+    /*
+        @brief
+        free all the resources
+    */
+    ~BigObjectAllocator();
+
+    /*
+        @brief
+        create a block of the specific size using a RedBlackTree to search the first free node and modify the node
+        @param size the size of the block
+        @return the void pointer to the block
+    */
+    void* Allocate(size_t size);
+    /*
+        @brief
+        free the block of a certain size and add or modify the corrisponding node in the RedBlackTree
+        @param size the size of the block
+    */
+    void Deallocate(void* ptr, size_t n);
+private:
+    /*
+        @brief
+        delete the node on the list and modify the RedBlackTree
+        @param address the searched address
+    */
+    void DeleteNodeOnList(char* address);
+    /*
+        @brief
+        modify the node on the list and modify the RedBlackTree
+        @param oldAddress the searched address
+        @param address the new address
+        @param newSize the new size
+    */
+    void ModifyNodeOnList(char* oldAddress, char* address, size_t newSize);
+    /*
+        @brief
+        add the node on the list and modify the RedBlackTree
+        @param address the address
+        @param newSize the size
+    */
+    void AddNodeOnList(char* address, size_t size);
+    /*
+        @brief
+        add the size to an axisting node and change the address if necessary
+        @param nodeOnList the node on the list
+        @param address the address of the node to merge
+        @param size the size of the node to merge
+    */
+    void MergeNodeOnList(FreeBlock* nodeOnList, char* address, size_t size);
+    /*
+        @brief
+        add the size to an axisting node and change the address if necessary considering also the next node on the list
+        @param nodeOnList the node on the list
+        @param address the address of the node to merge
+        @param size the size of the node to merge
+        @param nodeNextOnList the next node on the list
+    */
+    void MergeNodeOnList(FreeBlock* nodeOnList, char* address, size_t size, FreeBlock* nodeNextOnList);
+    size_t size;
+    size_t totalSize;
+    char* memoryBlock;
+    FreeBlock* firstNode;
+    RedBlackTree<char*, size_t>* freeRBtree;
+};
+
+
--- GeneralAllocator/RedBlackTree.hpp
+++ GeneralAllocator/RedBlackTree.hpp
@@ -1,312 +1,355 @@
 
-#pragma once
 #include <iostream>
-#include "framework.h"
+
 enum class Color { RED, BLACK };
 
-template <typename T>
-class GENERALALLOCATOR_API Node {
+template <typename T, typename V>
+class Node {
 public:
+    //the key identify the node
     T key;
+    //the val is the value used to create the tree
+    V val;
     Color color;
     Node* parent;
     Node* left;
     Node* right;
-    /*
-        @brief
-        Create a red node with a key 
-        @param key the key of the node
-    */
-    Node(T key) : key(key), color(Color::RED), parent(nullptr), left(nullptr), right(nullptr) {}
+
+    Node(T key, V value) : key(key), val(value), color(Color::RED), parent(nullptr), left(nullptr), right(nullptr) {}
+    ~Node()
+    {
+        if (left)
+            delete left;
+        if (right)
+            delete right;
+    }
 };
 
-template <typename T>
-class GENERALALLOCATOR_API RedBlackTree {
-private:
-    Node<T>* root;
-
-    /*
-        @brief
-        Helper function that Rotate the Tree to the left
-        @param x Node to Rotate
-    */
-    void LeftRotate(Node<T>* x);
-    /*
-        @brief
-        Helper function that Rotate the Tree to the right
-        @param x Node to Rotate
-    */
-    void RightRotate(Node<T>* x);
-    /*
-        @brief
-        Helper function that Insert the Node in the right place with the right color
-        @param x Node to insert
-    */
-    void InsertFixup(Node<T>* x);
-    /*
-        @brief
-        Helper function that replace a subtree with another updating the parent
-        @param u root Node of the first subtree
-        @param v root Node of the second subtree
-    */
-    void Transplant(Node<T>* u, Node<T>* v);
-    /*
-        @brief
-        Helper function to return the minimum node of the tree
-        @param node Node where to start to search the minimum
-        @return the minimum node
-    */
-    Node<T>* Minimum(Node<T>* node);
-    /*
-        @brief
-        Helper function to search a node with a specific key
-        @param node Node where to start to search
-        @param key the key to search
-        @return the searched node if exists
-    */
-    Node<T>* SearchHelper(Node<T>* node, T key);
+template <typename T, typename V>
+class RedBlackTree {
 public:
+    RedBlackTree();
+    ~RedBlackTree();
     /*
         @brief
-        Create a empty tree
+        Insert the node on the tree and balance the tree keeping the balance
+        @param key the key of the node
+        @param val the value of the node
     */
-    RedBlackTree() : root(nullptr) {}
+    void Insert(T key, V val);
     /*
         @brief
-        Insert the Node with a key
-        @param key the key to insert
+        Remove the node on the tree and balance the tree keeping the balance
+        @param key the key of the node
+        @param val the value of the node
     */
-    void Insert(T key);
+    void Remove(T key, V val);
     /*
         @brief
-        Remove the Node with a key
-        @param key the key to remove
+        Search the node on the tree based on the value
+        @param val the value of the node
     */
-    void Remove(T key);
+    Node<T, V>* Search(V val);
     /*
         @brief
-        Remove the Node with a key
-        @param key the key to remove
-        @return the searched node if exists
+        Clear the tree
     */
-    Node<T>* Search(T key);
+    void Clear();
+private:
+    int size;
+    Node<T, V>* root;
+    /*
+        @brief
+        Rotate the tree on the left based on a node as center
+        @param node the center node
+    */
+    void LeftRotate(Node<T, V>* node);
+    /*
+        @brief
+        Rotate the tree on the right based on a node as center
+        @param node the center node
+    */
+    void RightRotate(Node<T, V>* node);
+    void RemoveNode(Node<T, V>* node);
 };
 
-// Implementations of helper functions
-template <typename T>
-void RedBlackTree<T>::LeftRotate(Node<T>* x) {
-    if (!x || !x->right)
-        return; // Invalid rotation
+template <typename T, typename V>
+RedBlackTree<T, V>::RedBlackTree() :root(nullptr), size(0) {}
 
-    Node<T>* y = x->right; // Set y
-    x->right = y->left;   // Turn y's left subtree into x's right subtree
-
-    if (y->left)
-        y->left->parent = x;
-
-    y->parent = x->parent; // Link x's parent to y
-
-    if (!x->parent)
-        root = y;
-    else if (x == x->parent->left)
-        x->parent->left = y;
-    else
-        x->parent->right = y;
-
-    y->left = x;       // Put x on y's left
-    x->parent = y;
+template <typename T, typename V>
+RedBlackTree<T, V>::~RedBlackTree() {
+    if (root) {
+        delete root;
+    }
 }
 
-template <typename T>
-void RedBlackTree<T>::RightRotate(Node<T>* y) {
-    if (!y || !y->left)
-        return; // Invalid rotation
+template<typename T, typename V>
+void RedBlackTree<T, V>::Insert(T key, V val)
+{
 
-    Node<T>* x = y->left; // Set x
-    y->left = x->right;  // Turn x's right subtree into y's left subtree
+    Node<T, V>* node = new Node<T, V>(key, val);
 
-    if (x->right)
-        x->right->parent = y;
-
-    x->parent = y->parent; // Link y's parent to x
-
-    if (!y->parent)
-        root = x;
-    else if (y == y->parent->left)
-        y->parent->left = x;
-    else
-        y->parent->right = x;
-
-    x->right = y;    // Put y on x's right
-    y->parent = x;
-}
-
-template <typename T>
-void RedBlackTree<T>::InsertFixup(Node<T>* x) {
-    while (x != root && x->parent && x->parent->color == Color::RED) {
-        if (x->parent == x->parent->parent->left) {
-            Node<T>* y = x->parent->parent->right; // Uncle
-            if (y && y->color == Color::RED) {
-                // Case 1: x's uncle is red
-                x->parent->color = Color::BLACK;
-                y->color = Color::BLACK;
-                x->parent->parent->color = Color::RED;
-                x = x->parent->parent;
-            }
-            else {
-                if (x == x->parent->right) {
-                    // Case 2: x's uncle is black, and x is a right child
-                    x = x->parent;
-                    LeftRotate(x);
-                }
-                // Case 3: x's uncle is black, and x is a left child
-                x->parent->color = Color::BLACK;
-                x->parent->parent->color = Color::RED;
-                RightRotate(x->parent->parent);
-            }
-        }
-        else {
-            // Symmetric cases (x's parent is a right child)
-            Node<T>* y = x->parent->parent->left; // Uncle
-            if (y && y->color == Color::RED) {
-                // Case 1: x's uncle is red
-                x->parent->color = Color::BLACK;
-                y->color = Color::BLACK;
-                x->parent->parent->color = Color::RED;
-                x = x->parent->parent;
-            }
-            else {
-                if (x == x->parent->left) {
-                    // Case 2: x's uncle is black, and x is a left child
-                    x = x->parent;
-                    RightRotate(x);
-                }
-                // Case 3: x's uncle is black, and x is a right child
-                x->parent->color = Color::BLACK;
-                x->parent->parent->color = Color::RED;
-                LeftRotate(x->parent->parent);
-            }
-        }
-    }
-
-    root->color = Color::BLACK; // Ensure the root is black
-}
-
-// Implementation of public insertion method
-template <typename T>
-void RedBlackTree<T>::Insert(T key) {
-    Node<T>* newNode = new Node<T>(key);
-    Node<T>* current = root;
-    Node<T>* parent = nullptr;
-
-    // Find the parent node where the new node should be inserted
-    while (current != nullptr) {
-        parent = current;
-        if (key < current->key)
-            current = current->left;
-        else
-            current = current->right;
-    }
-
-    // Set the parent for the new node
-    newNode->parent = parent;
-
-    // If the tree is empty, make the new node the root
-    if (parent == nullptr) {
-        root = newNode;
-    }
-    else if (key < parent->key) {
-        parent->left = newNode;
-    }
-    else {
-        parent->right = newNode;
-    }
-
-    // Perform fix-up to maintain Red-Black Tree properties
-    InsertFixup(newNode);
-}
-template <typename T>
-void RedBlackTree<T>::Remove(T key) {
-    Node<T>* node = search(root, key);
-
-    if (!node) {
-        std::cout << "Node with key " << key << " not found." << std::endl;
+    if (root == nullptr) {
+        root = node;
+        node->color = Color::BLACK;
         return;
     }
 
-    Node<T>* y = node; // The node to be removed
-    Node<T>* x = nullptr; // Node that replaces y
-    Color yOriginalColor = y->color;
-
-    if (!node->left) {
-        x = node->right;
-        transplant(node, node->right);
-    }
-    else if (!node->right) {
-        x = node->left;
-        transplant(node, node->left);
-    }
-    else {
-        y = minimum(node->right);
-        yOriginalColor = y->color;
-        x = y->right;
-
-        if (y->parent == node)
-            x->parent = y;
-        else {
-            transplant(y, y->right);
-            y->right = node->right;
-            y->right->parent = y;
+    {
+        Node<T, V>* curr = root;
+        while (curr->left != nullptr || curr->right != nullptr)
+        {
+            if (val < curr->val)
+                curr = curr->left;
+            else
+                curr = curr->right;
         }
 
-        transplant(node, y);
-        y->left = node->left;
-        y->left->parent = y;
-        y->color = node->color;
+        node->parent = curr;
+        if (val < curr->val)
+            curr->left = node;
+        else
+            curr->right = node;
+
+        while (curr->color == Color::RED && curr->parent != nullptr)
+        {
+            Node<T, V>* bro;
+            if (curr == curr->parent->right)
+                bro = curr->parent->left;
+            else
+                bro = curr->parent->right;
+
+            if (bro == nullptr) {
+                curr->color = Color::BLACK;
+                curr->parent->color = Color::RED;
+                if (bro == curr->parent->right) {
+                    RightRotate(curr->parent);
+                }
+                else {
+                    LeftRotate(curr->parent);
+                }
+                break;
+            }
+            else if (bro->color == Color::RED) {
+                curr->color = Color::BLACK;
+                bro->color = Color::BLACK;
+                curr->parent->color = Color::RED;
+                curr = curr->parent;
+            }
+            else {
+                curr->color = Color::BLACK;
+                curr->parent->color = Color::RED;
+
+                if (curr == curr->parent->right) {
+                    if (node == curr->left) {
+                        RightRotate(curr);
+                        curr = node;
+                    }
+                    LeftRotate(curr->parent);
+                }
+                else {
+                    if (node == curr->right) {
+                        LeftRotate(curr);
+                        curr = node;
+                    }
+                    RightRotate(curr->parent);
+                }
+            }
+            root->color = Color::BLACK;
+        }
+
+        ++size;
     }
 
-    delete node;
 
-    if (yOriginalColor == Color::BLACK)
-        deleteFixup(x);
 }
 
-// Helper function to transplant a node
-template <typename T>
-void RedBlackTree<T>::Transplant(Node<T>* u, Node<T>* v) {
-    if (!u->parent)
-        root = v;
-    else if (u == u->parent->left)
-        u->parent->left = v;
+template<typename T, typename V>
+void RedBlackTree<T, V>::Remove(T key, V val)
+{
+    Node<T, V>* curr = root;
+    while (curr->left != nullptr || curr->right != nullptr)
+    {
+        if (curr->key == key)
+            break;
+
+        if (val >= curr->val)
+            curr = curr->right;
+        else
+            curr = curr->left;
+    }
+    if (curr->key != key)
+        return;
+
+    RemoveNode(curr);
+    --size;
+}
+
+template<typename T, typename V>
+void RedBlackTree<T, V>::RemoveNode(Node<T, V>* node)
+{
+    if (node->color == Color::RED) {
+        if (node->left != nullptr && node->right != nullptr) {
+            Node<T, V>* successor = node->right;
+            while (successor->left != nullptr)
+                successor = successor->left;
+            node->key = successor->key;
+            node->val = successor->val;
+            RemoveNode(successor);
+        }
+        else if (node->left != nullptr) {
+            node->key = node->left->key;
+            node->val = node->left->val;
+            node->color = node->left->color;
+            RemoveNode(node->left);
+        }
+        else if (node->right != nullptr) {
+            node->key = node->right->key;
+            node->val = node->right->val;
+            node->color = node->right->color;
+            RemoveNode(node->right);
+        }
+        else {
+            // no child
+            if (node->parent == nullptr) {
+                delete node;
+                root = nullptr;
+                return;
+            }
+
+            if (node->parent->left == node)
+                node->parent->left = nullptr;
+            else
+                node->parent->right = nullptr;
+
+            delete node;
+        }
+    }
+    else {
+        if (node->left != nullptr && node->right != nullptr) {
+            Node<T, V>* successor = node->right;
+            while (successor->left != nullptr)
+                successor = successor->left;
+            node->key = successor->key;
+            node->val = successor->val;
+            RemoveNode(successor);
+        }
+        else if (node->left != nullptr) {
+            node->key = node->left->key;
+            node->val = node->left->val;
+            RemoveNode(node->left);
+        }
+        else if (node->right != nullptr) {
+            node->key = node->right->key;
+            node->val = node->right->val;
+            RemoveNode(node->right);
+        }
+        else {
+            if (node->parent == nullptr) {
+                delete node;
+                root = nullptr;
+                return;
+            }
+
+            if (node->parent->left == node) {
+                node->parent->left = nullptr;
+                if (node->parent->right != nullptr
+                    && node->parent->right->color == Color::RED) {
+                    node->parent->right->color = Color::BLACK;
+                    LeftRotate(node->parent);
+                }
+            }
+            else {
+                node->parent->right = nullptr;
+                if (node->parent->left != nullptr
+                    && node->parent->left->color == Color::RED) {
+                    node->parent->left->color = Color::BLACK;
+                    LeftRotate(node->parent);
+                }
+            }
+
+            if (node->parent->left == nullptr
+                && node->parent->right == nullptr
+                && node->parent->parent != nullptr) {
+                LeftRotate(node->parent->parent);
+            }
+
+            delete node;
+        }
+    }
+}
+
+template<typename T, typename V>
+Node<T, V>* RedBlackTree<T, V>::Search(V val)
+{
+    Node<T, V>* curr = root;
+    while (curr->left != nullptr || curr->right != nullptr)
+    {
+        if (curr->val >= val) {
+            break;
+        }
+
+        if (val < curr->val)
+            curr = curr->left;
+        else
+            curr = curr->right;
+    }
+
+    if (curr->val < val)
+        return nullptr;
+    return curr;
+}
+
+template<typename T, typename V>
+void RedBlackTree<T, V>::LeftRotate(Node<T, V>* node)
+{
+    if (node->right != nullptr) return;
+    Node<T, V>* temp = node->right;
+
+    // update the two nodes
+    node->right = temp->left;
+    if (temp->left != nullptr)
+        temp->left->parent = node;
+    temp->left = node;
+    temp->parent = node->parent;
+    node->parent = temp;
+
+    // update the parent
+    if (root == node) {
+        root = temp;
+        return;
+    }
+    if (temp->parent->left == node)
+        temp->parent->left = temp;
     else
-        u->parent->right = v;
-
-    if (v)
-        v->parent = u->parent;
+        temp->parent->right = temp;
 }
+template<typename T, typename V>
+void RedBlackTree<T, V>::RightRotate(Node<T, V>* node)
+{
+    if (node->left != nullptr) return;
+    Node<T, V>* temp = node->left;
 
-// Helper function to find the minimum node in a subtree rooted at a given node
-template <typename T>
-Node<T>* RedBlackTree<T>::Minimum(Node<T>* node) {
-    while (node->left)
-        node = node->left;
-    return node;
-}
+    node->left = temp->right;
+    if (temp->right != nullptr)
+        temp->right->parent = node;
+    temp->right = node;
+    temp->parent = node->parent;
+    node->parent = temp;
 
-// Helper function to search for a node with a specific key
-template <typename T>
-Node<T>* RedBlackTree<T>::SearchHelper(Node<T>* node, T key) {
-    if (!node || node->key == key)
-        return node;
-
-    if (key < node->key)
-        return SearchHelper(node->left, key);
+    if (root == node) {
+        root = temp;
+        return;
+    }
+    if (temp->parent->left == node)
+        temp->parent->left = temp;
     else
-        return SearchHelper(node->right, key);
+        temp->parent->right = temp;
 }
-/*
-*/
-template <typename T>
-Node<T>* RedBlackTree<T>::Search(T key) {
-    return SearchHelper(root, key);
+
+template<typename T, typename V>
+void RedBlackTree<T, V>::Clear()
+{
+    delete root;
+    root = nullptr;
 }
