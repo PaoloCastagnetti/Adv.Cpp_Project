b446e4e0e966f1e334860cabecdaf49c54179594
Andrea Mocellin <andreamocellin2209@gmail.com>
2024-03-14 17:31:25 +0100

    Fixed a bit of stuff after Paolo's feedback

--- MemoryManager/MemoryManager.cpp
+++ MemoryManager/MemoryManager.cpp
@@ -2,8 +2,8 @@
 
 void MemoryManager::Init()
 {
-	bigObjAllocator = new BigObjectAllocator(_objTotalSize);
-	smallObjAllocator = new BigObjectAllocator(_objTotalSize);
+	bigObjAllocator = new BigObjectAllocator(_bigObjTotalSize);
+	smallObjAllocator = new SmallObjectAllocator(_smallObjPageSize, _smallObjTotalSize, _smallObjAlignSize);
 }
 
 void MemoryManager::Free()
@@ -23,7 +23,9 @@ void* MemoryManager::Allocate(std::size_t size)
 	// In this case it utilizes the SmallObjectAllocator
 	else
 	{
-		smallObjAllocator->Allocate(size);
+		// DEFAULT: do NOT throw exceptions
+		bool doThrow = true;
+		smallObjAllocator->Allocate(size, !doThrow);
 	}
 }
 
@@ -42,6 +44,63 @@ void MemoryManager::Deallocate(void* ptr, std::size_t size)
 	}
 }
 
+template<class NewT>
+NewT* MemoryManager::MM_New()
+{
+	// In this case it utilizes the BigObjectAllocator
+	if (size >= _sizeThreshold)
+	{
+		NewT newPtr = (NewT*)bigObjAllocator->Allocate(sizeof(NewT));
+		new(newPtr);
+
+		return newPtr;
+	}
+
+	// In this case it utilizes the SmallObjectAllocator
+	else
+	{
+		NewT newPtr = (NewT*)smallObjAllocator->Allocate(sizeof(NewT));
+		new(newPtr);
+
+		return newPtr;
+	}
+}
+
+template<class DeleteT>
+void MM_Delete(DeleteT* ptr)
+{
+	// Call the default destructor for type DeleteT
+	ptr->~DeleteT();
+
+	// In this case it utilizes the BigObjectAllocator
+	if (size >= _sizeThreshold)
+	{
+		bigObjAllocator->Deallocate(ptr, sizeof(NewT));
+	}
+
+	// In this case it utilizes the SmallObjectAllocator
+	else
+	{
+		smallObjAllocator->Deallocate(ptr, sizeof(NewT));
+	}
+
+	// Make the pointer null
+	ptr = nullptr;
+}
+
+// TODO
+template<class NewT>
+NewT* MM_New_A(std::size_t array_size)
+{
+
+}
+
+// TODO
+template<class DeleteT>
+void MM_Delete_A(DeleteT* ptr)
+{
+
+}
 
 // SIZE_THRESHOLD GETTER & SETTER
 
--- MemoryManager/MemoryManager.h
+++ MemoryManager/MemoryManager.h
@@ -3,95 +3,7 @@
 #include "BigObjectAllocator.h"
 #include "SmallObjectAllocator.h"
 
-
-class MemoryManager {
-public:
-	// no need for constructor in this class
-	MemoryManager() = delete;
-
-	// Instantiate SmallObjectAllocator and BigObjectAllocator
-	void Init();
-
-	// Free SmallObjectAllocator and BigObjectAllocator from memory
-	void Free();
-
-	void* Allocate(std::size_t size);
-	void Deallocate(void* ptr, std::size_t size);
-
-	size_t GetSizeThreshold();
-	void SetSizeThreshold(std::size_t sizeThreshold);
-
-	template<class NewT>
-	NewT* MM_New()
-	{
-		// In this case it utilizes the BigObjectAllocator
-		if (size >= _sizeThreshold)
-		{
-			NewT newPtr = (NewT*) bigObjAllocator->Allocate(sizeof(NewT));
-			new(newPtr);
-
-			return newPtr;
-		}
-
-		// In this case it utilizes the SmallObjectAllocator
-		else
-		{
-			NewT newPtr = (NewT*)smallObjAllocator->Allocate(sizeof(NewT));
-			new(newPtr);
-
-			return newPtr;
-		}
-	}
-
-	template<class DeleteT>
-	void MM_Delete(DeleteT* ptr)
-	{
-		// Call the default destructor for type DeleteT
-		ptr->~DeleteT();
-
-		// In this case it utilizes the BigObjectAllocator
-		if (size >= _sizeThreshold)
-		{
-			bigObjAllocator->Deallocate(ptr, sizeof(NewT));
-		}
-
-		// In this case it utilizes the SmallObjectAllocator
-		else
-		{
-			smallObjAllocator->Deallocate(ptr, sizeof(NewT));
-		}
-
-		// Make the pointer null
-		ptr = nullptr;
-	}
-
-	// TODO
-	template<class NewT>
-	NewT* MM_New_A(std::size_t array_size)
-	{
-
-	}
-
-	// TODO
-	template<class DeleteT>
-	void MM_Delete_A(DeleteT* ptr)
-	{
-
-	}
-
-private:
-	BigObjectAllocator* bigObjAllocator;
-
-	// WARNING: MISSING SMALL OBJ ALLOCATOR REFERENCE
-	BigObjectAllocator* smallObjAllocator;
-
-	std::size_t _sizeThreshold;
-	std::size_t _objTotalSize;
-};
-
-
-// macro definition
-
+// MACRO definition
 #ifndef MEMORY_MANAGER_MACROS
 #define MEMORY_MANAGER_MACROS
 
@@ -109,3 +21,47 @@ private:
 #	define MM_DELETE_ARRAY_T(T, ptr) ::MemoryManager::MM_Delete_A<T>(ptr)
 
 #endif
+
+
+class MemoryManager {
+public:
+	// no need for constructor in this class
+	MemoryManager() = delete;
+
+	// Instantiate SmallObjectAllocator and BigObjectAllocator
+	void Init();
+
+	// Free SmallObjectAllocator and BigObjectAllocator from memory
+	void Free();
+
+	void* Allocate(std::size_t size);
+	void Deallocate(void* ptr, std::size_t size);
+
+	template<class NewT>
+	NewT* MM_New();
+
+	template<class DeleteT>
+	void MM_Delete(DeleteT* ptr);
+
+	template<class NewT>
+	NewT* MM_New_A(std::size_t array_size);
+
+	template<class DeleteT>
+	void MM_Delete_A(DeleteT* ptr);
+
+	std::size_t GetSizeThreshold();
+	void SetSizeThreshold(std::size_t sizeThreshold);
+
+private:
+	BigObjectAllocator* bigObjAllocator;
+
+	SmallObjectAllocator* smallObjAllocator;
+
+	std::size_t _sizeThreshold;
+
+	std::size_t _bigObjTotalSize;
+
+	std::size_t	_smallObjPageSize;
+	std::size_t _smallObjTotalSize;
+	std::size_t _smallObjAlignSize;
+};
